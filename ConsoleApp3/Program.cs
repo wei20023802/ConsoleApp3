b62963b1ba82dca0d9ba781b99c56088de369cae
user <user@DESKTOP-6JBEF5K>
2022-03-07 12:04:05 +0800

    加入專案檔案。

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double meal_cost = 100;
+            int tip_percent = 15;
+            int tax_percent = 8;
+
+            double summeal = meal_cost + tip_percent + tax_percent;
+            Console.WriteLine("用餐總費用：" + summeal);
+        }
+    }
+}
